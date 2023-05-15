1c8db09a4b770a8b169eb6514721fdf00f7196b3
Muze-mac <13581261474@163.com>
2023-05-15 15:50:54 +0800

    可用

--- vite/admin/src/components/ShowList.vue
+++ vite/admin/src/components/ShowList.vue
@@ -1,13 +1,10 @@
 <script setup>
 import ShowChart from "./ShowChart.vue";
-import { computed, ref } from "vue";
+import { ref, computed } from "vue";
 
 const props = defineProps({
   data: Array,
 });
-
-// 表头常量
-const COLUMNS = ["Id", "计划", "次数", "类型", "时间"];
 
 // 定义一个去重处理函数
 const distinct = (arr) => [...new Set(arr)];
@@ -21,121 +18,87 @@ const filterRowsById = (rows, id) => {
   return rows.filter((row) => row.id.toString() === id);
 };
 
-// 响应式变量
-const filterType = ref(0);
-const selectedId = ref("");
-const inputId = ref("");
-
-// 计算属性
-const rows = computed(() =>
-  props.data
+// 数据部分
+const data = computed(() => {
+  const rows = props.data
     .map((item) => ({
       id: item.id,
       计划: "暂无",
       count: item.count,
-      类型: "点击",
-      date: item.date.slice(8),
+      type: item.type,
+      date: item.date.slice(5),
     }))
-    .sort((a, b) => b.date - a.date)
-);
-
-const filteredRows = computed(() => {
-  const rowsToFilter = filterType.value === 0 ? rows.value : [...rows.value];
-  return filterRowsById(
-    rowsToFilter,
-    filterType.value === 0 ? selectedId.value : inputId.value
-  );
+    .sort((a, b) => a.date - b.date);
+  const distinctIds = distinct(rows.map((row) => row.id));
+  return { rows, distinctIds };
 });
-const distinctIds = computed(() => distinct(rows.value.map((row) => row.id)));
 
-// 点击事件处理程序
-const handleFilterTypeChange = (type) => {
-  filterType.value = type;
-  if (type === 0) {
-    selectedId.value = "";
-  } else {
-    inputId.value = "";
-  }
-};
+// 模板部分
+const selectedId = ref("");
+const templateData = computed(() => {
+  const { rows, distinctIds } = data.value;
+  const filteredRows = filterRowsById(rows, selectedId.value);
+  return {
+    COLUMNS: ["Id", "计划", "次数", "类型", "时间"],
+    rows: filteredRows,
+    distinctIds: [""].concat(distinctIds),
+  };
+});
 </script>
 
 <template>
   <div class="content">
-    <!-- 添加选项卡，用于选择筛选方式 -->
-    <div class="filter-type">
-      <button
-        :class="{ active: filterType === 0 }"
-        @click="handleFilterTypeChange(0)"
-      >
-        现有 ID
-      </button>
-      <button
-        :class="{ active: filterType === 1 }"
-        @click="handleFilterTypeChange(1)"
-      >
-        输入 ID
-      </button>
-    </div>
-
-    <!-- 如果选择了“现有 ID”，则显示一个下拉列表，供用户选择已有的 ID -->
-    <div v-if="filterType === 0" class="filter-dropdown">
-      <label for="selected-id">请选择现有 ID：</label>
+    <!-- 下拉列表选择ID -->
+    <div class="filter-dropdown">
+      <label for="selected-id">请选择ID：</label>
       <select id="selected-id" v-model="selectedId">
-        <option value="">全部</option>
-        <option v-for="id in distinctIds" :key="id" :value="id">
+        <option v-for="id in templateData.distinctIds" :key="id" :value="id">
           {{ id }}
         </option>
       </select>
-    </div>
-
-    <!-- 如果选择了“输入 ID”，则显示一个输入框，供用户输入要查询的 ID -->
-    <div v-else class="filter-input">
-      <label for="input-id">请输入要查询的 ID：</label>
-      <input type="text" id="input-id" v-model="inputId" />
     </div>
 
     <!-- 表格部分 -->
     <table class="widefat" width="300px">
       <thead>
         <tr>
-          <th v-for="column in COLUMNS" :key="column">{{ column }}</th>
+          <th v-for="column in templateData.COLUMNS" :key="column">
+            {{ column }}
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(row, index) in filteredRows" :key="index">
+        <tr v-for="(row, index) in templateData.rows" :key="index">
           <td v-for="(value, key) in row" :key="key">{{ value }}</td>
         </tr>
       </tbody>
     </table>
   </div>
-<!---->
-  <ShowChart :data="filteredRows"></ShowChart>
-  
+  <!---->
+  <ShowChart :data="templateData.rows"></ShowChart>
 </template>
 
 <style scoped>
-.active {
-  background-color: #2b6cb0;
-  color: #fff;
-}
-
-.filter-type,
-.filter-dropdown,
-.filter-input {
-  margin-bottom: 20px;
-}
-
 label {
   display: inline-block;
   width: 120px;
 }
 
-input[type="text"],
 select {
   width: 200px;
   padding: 5px;
   border-radius: 5px;
   outline: none;
   border: 1px solid #ccc;
+}
+
+table {
+  margin-top: 20px;
+}
+
+th,
+td {
+  padding: 5px;
+  text-align: center;
 }
 </style>
