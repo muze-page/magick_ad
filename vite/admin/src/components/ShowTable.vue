c789de0b7b8ba899d17385fa6e8b6f3921605a71
Muze-mac <13581261474@163.com>
2023-05-16 19:32:15 +0800

    调整时间格式

--- vite/admin/src/components/ShowTable.vue
+++ vite/admin/src/components/ShowTable.vue
@@ -1,11 +1,25 @@
-<script lang="ts" setup>
+<script setup>
 //展示表格
+import { computed } from "vue";
 const props = defineProps({
   data: Array,
 });
+const rows = computed(() =>
+  props.data.map((item) => {
+    //调整时间
+    const [year, month, day] = item.date.split("-");
+    return {
+      id: item.id,
+      计划: "暂无",
+      count: item.count,
+      type: item.type,
+      date: `${month}-${day}`, // 将时间字符串改为 02-14 格式
+    };
+  })
+);
 </script>
 <template>
-  <el-table :data="props.data" stripe style="width: 100%" max-height="300">
+  <el-table :data="rows" stripe style="width: 100%" max-height="300">
     <el-table-column fixed prop="id" label="ID" />
     <el-table-column prop="计划" label="计划" />
     <el-table-column prop="count" label="次数" />
