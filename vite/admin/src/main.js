9d1a4c50bc842946c8a22ff56cd9693b8169be53
Muze-mac <13581261474@163.com>
2023-05-27 13:54:07 +0800

    打包更改

--- vite/admin/src/main.js
+++ vite/admin/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from "vue";
-import "./style.css";
+
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 
+/**
+ * 测试样式
+ */
+//import "./style.css";
 /**
  * 测试数据
  */
